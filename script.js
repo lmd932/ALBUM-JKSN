b0cf8b41325004d69e37b64bb36b043bfbeda538
lmd932 <lehny932@gmail.com>
2024-11-14 00:27:56 +0100

    Album v2

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-// Fonction pour afficher les détails de l'album dans une fenêtre modale
-function showDetails(element) {
-    const modal = document.getElementById("modal");
-    const modalImage = document.getElementById("modal-image");
-    const modalTitle = document.getElementById("modal-title");
-    const modalDescription = document.getElementById("modal-description");
-
-    // Récupération des informations de l'album
-    const imgSrc = element.querySelector("img").src;
-    const title = element.querySelector("p").innerText;
-
-    // Mise à jour du contenu de la modale
-    modalImage.src = imgSrc;
-    modalTitle.innerText = title;
-    modalDescription.innerText = ` ${title} jeune rappeur du 93 sort son première album nommé CRACK qui contient 9 morceaux`;
-
-    // Affichage de la modale
-    modal.style.display = "flex";
+function showTrackList(element) {
+    const cover = element.querySelector('.album-cover');
+    const trackList = element.querySelector('.track-list');
+    
+    cover.style.opacity = "0";
+    trackList.style.opacity = "1";
 }
 
-// Fonction pour fermer la fenêtre modale
-function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
+function hideTrackList(element) {
+    const cover = element.querySelector('.album-cover');
+    const trackList = element.querySelector('.track-list');
+    
+    cover.style.opacity = "1";
+    trackList.style.opacity = "0";
 }
-
-// Fermer la modale en cliquant à l'extérieur du contenu
-window.onclick = function(event) {
-    const modal = document.getElementById("modal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
